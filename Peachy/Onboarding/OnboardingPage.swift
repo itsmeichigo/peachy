02cbfd27e0be8aa128650075e79dcfd032a2b519
Huong Do <hi.huongdo@gmail.com>
2022-02-04 00:18:02 +0700

    Update UI for permission view

--- Peachy/Onboarding/OnboardingPage.swift
+++ Peachy/Onboarding/OnboardingPage.swift
@@ -1,55 +1,24 @@
-//
-//  OnboardingPage.swift
-//  cmd
-//
-//  Created by Huong Do on 03/03/2021.
-//
-
 import Foundation
 import SwiftUI
 
 enum OnboardingPage: CaseIterable {
-  case welcome
-  case feature
-  case permission
-  case settings
-  
-  static let freshOnboarding = OnboardingPage.allCases
-  
-  var shouldShowPreviousButton: Bool {
-    switch self {
-    case .feature, .permission, .settings:
-      return true
-    case .welcome:
-      return false
+    case permission
+    case settings
+    
+    static let freshOnboarding = OnboardingPage.allCases
+    
+    var view: some View {
+        switch self {
+        case .permission:
+            return PermissionView().eraseToAnyView()
+        case .settings:
+            return SettingsView().eraseToAnyView()
+        }
     }
-  }
-  
-  var shouldShowNextButton: Bool {
-    switch self {
-    case .welcome, .feature, .permission:
-      return true
-    case .settings:
-      return false
-    }
-  }
-  
-  var view: some View {
-    switch self {
-    case .welcome:
-      return WelcomeView().eraseToAnyView()
-    case .feature:
-      return FeatureView().eraseToAnyView()
-    case .permission:
-      return PermissionView().eraseToAnyView()
-    case .settings:
-      return SettingsView().eraseToAnyView()
-    }
-  }
 }
 
 extension View {
-  func eraseToAnyView() -> AnyView {
-    AnyView(self)
-  }
+    func eraseToAnyView() -> AnyView {
+        AnyView(self)
+    }
 }
--- Peachy/Onboarding/Views/PermissionView.swift
+++ Peachy/Onboarding/Views/PermissionView.swift
@@ -2,12 +2,32 @@ import SwiftUI
 
 struct PermissionView: View {
     var body: some View {
-        Text("Peachy needs Accessibility access.")
-            .font(.title)
-        
-        Button("Open System Preferences") {
-            NSWorkspace.shared.open(
-                URL(string: "x-apple.systempreferences:com.apple.preference.security?Privacy_Accessibility")!)
+        VStack {
+            HStack(alignment: .center) {
+                Image("peach-colored")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 24)
+                Text("Let's get started")
+                    .font(.largeTitle)
+            }
+            .padding(.bottom, 24)
+            
+            Text("Peachy needs access to your computer's Accessibility features to work.")
+                .font(.title3)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .frame(width: 320)
+
+            Image("accessibility")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 320)
+            
+            Button("Grant Access") {
+                NSWorkspace.shared.open(
+                    URL(string: "x-apple.systempreferences:com.apple.preference.security?Privacy_Accessibility")!)
+            }
         }
     }
 }
