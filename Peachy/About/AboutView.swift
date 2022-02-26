6db9044c6b400bca14073ca163679d6742d6cdb5
Huong Do <hi.huongdo@gmail.com>
2022-02-26 16:05:49 +0700

    Add copyright to about screen

--- Peachy/About/AboutView.swift
+++ Peachy/About/AboutView.swift
@@ -4,12 +4,18 @@ struct AboutView: View {
     var body: some View {
         VStack {
             Image("small-icon")
+                .resizable()
+                .frame(width: 120, height: 120)
             Text("Peachy")
                 .font(.title)
                 .bold()
             Text("Version \(Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String)")
                 .foregroundColor(.secondary)
+            Spacer()
+            Text("© 2022 Huong Do. All rights reserved.")
+                .foregroundColor(.secondary)
         }
+        .padding(16)
         .frame(width: 320, height: 240)
         .background(Color(NSColor.windowBackgroundColor))
     }
