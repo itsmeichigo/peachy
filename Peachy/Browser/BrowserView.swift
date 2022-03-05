26e30b69f91548b900269d35f9c4688cb9671efc
Huong Do <hi.huongdo@gmail.com>
2022-03-05 15:51:49 +0700

    Add search bar to browser view

--- Peachy/Browser/BrowserView.swift
+++ Peachy/Browser/BrowserView.swift
@@ -1,12 +1,47 @@
 import SwiftUI
 
 struct BrowserView: View {
+    private let kaomojiStore = KaomojiStore.shared
+    @State private var query: String = ""
+    
     var body: some View {
-        HStack {
-            Text("Tags")
-            Text("Grid")
+        NavigationView {
+            VStack {
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .frame(width: 16, height: 16)
+                    TextField("Search", text: $query)
+                        .textFieldStyle(.plain)
+                    if !query.isEmpty {
+                        Button {
+                            query = ""
+                        } label: {
+                            Image(systemName: "xmark.circle.fill")
+                                .resizable()
+                                .frame(width: 16, height: 16)
+                                .foregroundColor(Color(NSColor.lightGray))
+                        }
+                        .buttonStyle(.plain)
+                    }
+                }
+                .padding(8)
+                .background(Color(NSColor.textBackgroundColor))
+                .cornerRadius(10)
+                .padding(8)
+
+                Spacer()
+            }
         }
-        .frame(width: 640, height: 480)
-        .background(Color(NSColor.windowBackgroundColor))
+        .frame(minWidth: 640, minHeight: 480)
+        .toolbar {
+            ToolbarItem(placement: .navigation) {
+                Button {
+                    NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
+                } label: {
+                    Image(systemName: "sidebar.leading")
+                }
+            }
+        }
     }
 }
