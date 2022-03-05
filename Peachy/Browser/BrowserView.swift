9f60e50038732a3566800006e0f3a22739361f5b
Huong Do <hi.huongdo@gmail.com>
2022-03-05 16:51:01 +0700

    Add tag list to side bar

--- Peachy/Browser/BrowserView.swift
+++ Peachy/Browser/BrowserView.swift
@@ -3,35 +3,26 @@ import SwiftUI
 struct BrowserView: View {
     private let kaomojiStore = KaomojiStore.shared
     @State private var query: String = ""
+    @State private var selectedTag: String?
     
     var body: some View {
         NavigationView {
-            VStack {
-                HStack {
-                    Image(systemName: "magnifyingglass")
-                        .resizable()
-                        .frame(width: 16, height: 16)
-                    TextField("Search", text: $query)
-                        .textFieldStyle(.plain)
-                    if !query.isEmpty {
-                        Button {
-                            query = ""
-                        } label: {
-                            Image(systemName: "xmark.circle.fill")
-                                .resizable()
-                                .frame(width: 16, height: 16)
-                                .foregroundColor(Color(NSColor.lightGray))
-                        }
-                        .buttonStyle(.plain)
+            VStack(alignment: .leading) {
+                searchBar
+                
+                List(selection: $selectedTag) {
+                    Text("TAGS")
+                        .bold()
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                    ForEach(kaomojiStore.kaomojiTags, id: \.self) { tag in
+                        Text("#\(tag)")
+                            .padding(.leading, 8)
                     }
                 }
-                .padding(8)
-                .background(Color(NSColor.textBackgroundColor))
-                .cornerRadius(10)
-                .padding(8)
-
-                Spacer()
+                .listStyle(.sidebar)
             }
+            Text("Content")
         }
         .frame(minWidth: 640, minHeight: 480)
         .toolbar {
@@ -43,5 +34,30 @@ struct BrowserView: View {
                 }
             }
         }
+    }
+
+    private var searchBar: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .resizable()
+                .frame(width: 12, height: 12)
+            TextField("Search", text: $query)
+                .textFieldStyle(.plain)
+            if !query.isEmpty {
+                Button {
+                    query = ""
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .resizable()
+                        .frame(width: 12, height: 12)
+                        .foregroundColor(Color(NSColor.lightGray))
+                }
+                .buttonStyle(.plain)
+            }
+        }
+        .padding(8)
+        .background(Color(NSColor.textBackgroundColor))
+        .cornerRadius(10)
+        .padding([.horizontal, .top], 8)
     }
 }
